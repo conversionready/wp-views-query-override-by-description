54ca953121b9b994c47a620370948e942226b1c0
leho <leho@conversionready.com>
2020-02-24 22:54:56 +0200

    2020.02.24

--- plugin.php
+++ plugin.php
@@ -1,12 +1,12 @@
 <?php
-/*
-Plugin Name: Toolset Views Query Override By Description
-Plugin URI: https://wp-types.com/forums/topic/non-public-post-type-is-not-listed-for-views-selection/
-Description: Views gains ability to modify final query arguments with description field JSON spec. Example `{ "query": { "orderby": "include" } }`
-Version: 2016.05.17
-Author: Leho Kraav
-Author URI: https://conversionready.com
-*/
+/**
+ * Plugin Name: Toolset Views Query Override By Description
+ * Plugin URI: https://wp-types.com/forums/topic/non-public-post-type-is-not-listed-for-views-selection/
+ * Description: Views gains ability to modify final query arguments with description field JSON spec. Example `{ "query": { "orderby": "include" } }`
+ * Version: 2020.02.24
+ * Author: Leho Kraav
+ * Author URI: https://conversionready.com
+ */
 
 if ( is_admin() ) {
     return;
@@ -16,23 +16,23 @@ final class WPV_Query_Override_Description {
 
     public static function plugins_loaded() {
 
-        if ( ! defined( "WPV_VERSION" ) ) {
+        if ( ! defined( 'WPV_VERSION' ) ) {
             return;
         }
 
         # plugins/wp-views/embedded/inc/wpv-filter-users-embedded.php wpv_users_query_user_filters()
-        add_filter( "wpv_filter_query", [ __CLASS__, "wpv_filter_query" ], 61, 3 );
-        add_filter( "wpv_filter_taxonomy_query", [ __CLASS__, "wpv_filter_query" ], 61, 3 );
-        add_filter( "wpv_filter_user_query", [ __CLASS__, "wpv_filter_query" ], 61, 3 );
+        add_filter( 'wpv_filter_query', [ __CLASS__, 'wpv_filter_query' ], 61, 3 );
+        add_filter( 'wpv_filter_taxonomy_query', [ __CLASS__, 'wpv_filter_query' ], 61, 3 );
+        add_filter( 'wpv_filter_user_query', [ __CLASS__, 'wpv_filter_query' ], 61, 3 );
 
     }
 
     public static function wpv_filter_query( $query, $settings, $view_id ) {
 
-        $settings["view_description"] = json_decode( get_post_meta( $view_id, "_wpv_description", true ), true );
+        $settings['view_description'] = json_decode( get_post_meta( $view_id, '_wpv_description', true ), true );
 
-        if ( is_array( $settings["view_description"] ) && isset( $settings["view_description"]["query"] ) ) {
-            $query = array_merge( $query, $settings["view_description"]["query"] );
+        if ( is_array( $settings['view_description'] ) && isset( $settings['view_description']['query'] ) ) {
+            $query = array_merge( $query, $settings['view_description']['query'] );
         }
 
         return $query;
@@ -41,4 +41,4 @@ final class WPV_Query_Override_Description {
 
 }
 
-add_action( "plugins_loaded", [ "WPV_Query_Override_Description", "plugins_loaded" ] );
+add_action( 'plugins_loaded', [ 'WPV_Query_Override_Description', 'plugins_loaded' ] );
